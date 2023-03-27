b2ee1e83da4a863a717a13e1db2e94d608e4df05
Advait Naik <ad.naik0801@gmail.com>
2023-03-27 19:25:34 +0530

    Initial Commmit

---
+++ Hello_World/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+//void main is the starting stage of any flutter apps
+void main(){
+  runApp(
+      const MaterialApp(
+        home: Center(
+          child: Text(
+            "Hello World!!!"
+          ),
+        ),
+      )
+  );
+}
